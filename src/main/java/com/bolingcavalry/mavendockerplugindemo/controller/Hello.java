7f857c0c767ebc2b22dce2e5ab14c61ada0a7df9
reversema <727798928@qq.com>
2022-01-18 20:12:34 +0800

    initial

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -8,7 +8,7 @@ import java.util.Date;
 @RestController
 public class Hello {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String sayHello(){
     	//修改返回的字符串的内容
         return "123456. Hello jenkins, " + new Date();
