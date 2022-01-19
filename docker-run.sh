671d6c97fa28a3856afe698f470bafc3284e8e4a
reversema <727798928@qq.com>
2022-01-19 17:32:04 +0800

    initial

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,7 @@
+echo "退出docker"
 exit
+echo "开始构建镜像"
 docker build -t com.bolingcavalry.mavendockerplugindemo /jenkinshome/workspace/AgileSoftware
+echo "构建结束，开始运行"
 docker run com.bolingcavalry.mavendockerplugindemo -p 8080:8080
+echo "运行成功"
